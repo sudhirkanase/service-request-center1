4245c3269f9379d6b137b63ffecd95a3972122cc
Heta <hetashah95@gmail.com>
2020-07-24 18:38:13 +0530

    Contact center API added

---
+++ task_management/src/main/java/com/wellsfargo/serv_req_center/task_management/beans/ContactCenterDetail.java
@@ -0,0 +1,79 @@
+package com.wellsfargo.serv_req_center.task_management.beans;
+
+import java.io.Serializable;
+/**
+ * Contact Center Detail
+ * @author Heta Shah
+ *
+ */
+public class ContactCenterDetail extends ServiceRequestTask implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String callerName;
+	private String callerPhone;
+	private String callCode;
+	private String action;
+	private String fullyAuthenticated;
+	private String taxpayerId;
+	private String taskPriority;
+	private String taskNotes;
+	private String callDetails;
+	
+	public String getCallerName() {
+		return callerName;
+	}
+	public void setCallerName(String callerName) {
+		this.callerName = callerName;
+	}
+	public String getCallerPhone() {
+		return callerPhone;
+	}
+	public void setCallerPhone(String callerPhone) {
+		this.callerPhone = callerPhone;
+	}
+	public String getCallCode() {
+		return callCode;
+	}
+	public void setCallCode(String callCode) {
+		this.callCode = callCode;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getFullyAuthenticated() {
+		return fullyAuthenticated;
+	}
+	public void setFullyAuthenticated(String fullyAuthenticated) {
+		this.fullyAuthenticated = fullyAuthenticated;
+	}
+	public String getTaxpayerId() {
+		return taxpayerId;
+	}
+	public void setTaxpayerId(String taxpayerId) {
+		this.taxpayerId = taxpayerId;
+	}
+	public String getTaskPriority() {
+		return taskPriority;
+	}
+	public void setTaskPriority(String taskPriority) {
+		this.taskPriority = taskPriority;
+	}
+	public String getTaskNotes() {
+		return taskNotes;
+	}
+	public void setTaskNotes(String taskNotes) {
+		this.taskNotes = taskNotes;
+	}
+	public String getCallDetails() {
+		return callDetails;
+	}
+	public void setCallDetails(String callDetails) {
+		this.callDetails = callDetails;
+	}
+	
+	
+}
--- task_management/src/main/java/com/wellsfargo/serv_req_center/task_management/beans/ServiceRequestTask.java
+++ task_management/src/main/java/com/wellsfargo/serv_req_center/task_management/beans/ServiceRequestTask.java
@@ -1,7 +1,15 @@
 package com.wellsfargo.serv_req_center.task_management.beans;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+/**
+ * Base class for all Tasks
+ * Includes Common fields related to all tasks
+ * @author Heta Shah
+ *
+ */
 public class ServiceRequestTask implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private long id;
@@ -12,13 +20,25 @@ public class ServiceRequestTask implements Serializable {
 	private String accountName;
 	private String requesterName;
 	private String dueDate;
+	private String status;
+	private String phone;
+	private String createdDate;
+	private Account accountDetail;
+	private String email;
+	private String assignedUserGroup;
+	private String assignedEmail;
+	private String accountService;
+	private List<Document> documents;
 
 	public ServiceRequestTask() {
 
 	}
 
 	public ServiceRequestTask(long id, String taskType, String taskSpecific, String workflowStep, String accountNo,
-			String accountName, String requesterName, String dueDate) {
+			String accountName, String requesterName, String dueDate, String status, String phone, String createdDate,
+			Account accountDetail, String email, String assignedUserGroup, String assignedEmail, String accountService,
+			List<Document> documents) {
+		super();
 		this.id = id;
 		this.taskType = taskType;
 		this.taskSpecific = taskSpecific;
@@ -27,6 +47,15 @@ public class ServiceRequestTask implements Serializable {
 		this.accountName = accountName;
 		this.requesterName = requesterName;
 		this.dueDate = dueDate;
+		this.status = status;
+		this.phone = phone;
+		this.createdDate = createdDate;
+		this.accountDetail = accountDetail;
+		this.email = email;
+		this.assignedUserGroup = assignedUserGroup;
+		this.assignedEmail = assignedEmail;
+		this.accountService = accountService;
+		this.documents = documents;
 	}
 
 	public long getId() {
@@ -91,6 +120,78 @@ public class ServiceRequestTask implements Serializable {
 
 	public void setDueDate(String dueDate) {
 		this.dueDate = dueDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Account getAccountDetail() {
+		return accountDetail;
+	}
+
+	public void setAccountDetail(Account accountDetail) {
+		this.accountDetail = accountDetail;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAssignedUserGroup() {
+		return assignedUserGroup;
+	}
+
+	public void setAssignedUserGroup(String assignedUserGroup) {
+		this.assignedUserGroup = assignedUserGroup;
+	}
+
+	public String getAssignedEmail() {
+		return assignedEmail;
+	}
+
+	public void setAssignedEmail(String assignedEmail) {
+		this.assignedEmail = assignedEmail;
+	}
+
+	public String getAccountService() {
+		return accountService;
+	}
+
+	public void setAccountService(String accountService) {
+		this.accountService = accountService;
+	}
+
+	public List<Document> getDocuments() {
+		return documents;
+	}
+
+	public void setDocuments(List<Document> documents) {
+		this.documents = documents;
 	}
 
 }
