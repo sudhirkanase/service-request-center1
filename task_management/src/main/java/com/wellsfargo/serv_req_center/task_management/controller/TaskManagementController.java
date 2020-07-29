04ceeae7d93def7538e00a0b4b60950dbe785009
Sudhir Kanase <sudhirkanase@gmail.com>
2020-07-29 22:08:45 +0530

    File Upload folder structure temp

--- task_management/src/main/java/com/wellsfargo/serv_req_center/task_management/controller/TaskManagementController.java
+++ task_management/src/main/java/com/wellsfargo/serv_req_center/task_management/controller/TaskManagementController.java
@@ -1,5 +1,6 @@
 package com.wellsfargo.serv_req_center.task_management.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wellsfargo.serv_req_center.task_management.beans.ServiceRequestTask;
 import com.wellsfargo.serv_req_center.task_management.service.TaskManagementService;
 
@@ -24,40 +23,53 @@ import com.wellsfargo.serv_req_center.task_management.service.TaskManagementServ
 //@RequestMapping("")
 public class TaskManagementController {
 
-	
 	@Autowired
 	TaskManagementService taskService;
-	
-	private static String UPLOAD_FOLDER = "G://SynechronTest//";
+
+	// Temp path for file upload
+	private static String UPLOAD_FOLDER = "file_upload";
 
 	@GetMapping("/getServiceReqTasks")
 	public ResponseEntity<List<ServiceRequestTask>> getServiceReqTasks() {
 		return ResponseEntity.ok(taskService.getServiceReqTasks());
 	}
-	
-	@PostMapping("/documentUpload")
-	  public ResponseEntity<String> documentUpload(@RequestParam("file") MultipartFile file) {
-	    String message = "";
-	    try {
-	      save(file);
-	      message = "Uploaded the file successfully: " + file.getOriginalFilename();
-	      return ResponseEntity.status(HttpStatus.OK).body(message);
-	    } catch (Exception e) {
-	      message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-	      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
-	    }
-	  }
 
-	
-	
+	@PostMapping("/documentUpload")
+	public ResponseEntity<String> documentUpload(@RequestParam("file") MultipartFile file) {
+		String message = "";
+		try {
+			save(file);
+			message = "Uploaded the file successfully: " + file.getOriginalFilename();
+			return ResponseEntity.status(HttpStatus.OK).body(message);
+		} catch (Exception e) {
+			message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+		}
+	}
+
 	public void save(MultipartFile file) {
-	    try {
-	    	byte[] bytes = file.getBytes();
-			Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
-			Files.write(path, bytes);
-	    } catch (Exception e) {
-	      throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
-	    }
-	  }
+		try {
+			byte[] bytes = file.getBytes();
+			Path uploadFilePath = getUploadFilePath(file.getOriginalFilename());
+			Files.write(uploadFilePath, bytes);
+		} catch (Exception e) {
+			throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+		}
+	}
+
+	private Path getUploadFilePath(String fileName) throws IOException {
+		
+		// <TODO> File Folder existance should be
+		Path fileUploadFolder = Paths
+				.get(Paths.get(".").normalize().toAbsolutePath().getParent() + File.separator + UPLOAD_FOLDER);
+		// Path path = Paths.get(Paths.get(".").normalize().toAbsolutePath()+ "\\test\\"
+		// + file.getOriginalFilename());
+		if (!Files.isDirectory(fileUploadFolder)) {
+			Files.createDirectories(fileUploadFolder);
+		}
+		System.out.println("File upload path:- " + fileUploadFolder.toString() );
+		Path uploadFilePath = Paths.get(fileUploadFolder + File.separator + fileName);
+		return uploadFilePath;
+	}
 
 }
--- task_management/src/test/java/com/wellsfargo/serv_req_center/task_management/TaskManagementApplicationTests.java
+++ task_management/src/test/java/com/wellsfargo/serv_req_center/task_management/TaskManagementApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TaskManagementApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
